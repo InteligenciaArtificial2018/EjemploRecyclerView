8c76ee1137de8357e0c50df580c5fea6ce646d13
Hector Sabillon <h.hsabillon@gmail.com>
2018-11-28 11:27:41 -0600

    new advance brach for testing

--- app/src/main/java/tech/inversa/ejemplorecyclerview/MainActivity.kt
+++ app/src/main/java/tech/inversa/ejemplorecyclerview/MainActivity.kt
@@ -2,11 +2,37 @@ package tech.inversa.ejemplorecyclerview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    var lista: RecyclerView? = null
+    var adaptador
+    var layoutManager: RecyclerView.LayoutManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val platillos = ArrayList<Platillo>()
+
+        platillos.add(Platillo("Platillo 1", 250.0, 3.5, R.drawable.platillo01))
+        platillos.add(Platillo("Platillo 1", 250.0, 3.5, R.drawable.platillo02))
+        platillos.add(Platillo("Platillo 1", 250.0, 3.5, R.drawable.platillo03))
+        platillos.add(Platillo("Platillo 1", 250.0, 3.5, R.drawable.platillo04))
+        platillos.add(Platillo("Platillo 1", 250.0, 3.5, R.drawable.platillo05))
+        platillos.add(Platillo("Platillo 1", 250.0, 3.5, R.drawable.platillo06))
+        platillos.add(Platillo("Platillo 1", 250.0, 3.5, R.drawable.platillo07))
+        platillos.add(Platillo("Platillo 1", 250.0, 3.5, R.drawable.platillo08))
+        platillos.add(Platillo("Platillo 1", 250.0, 3.5, R.drawable.platillo09))
+        platillos.add(Platillo("Platillo 1", 250.0, 3.5, R.drawable.platillo10))
+
+        lista = findViewById(R.id.rvLista)
+        lista?.setHasFixedSize(true)
+
+        layoutManager = LinearLayoutManager(this)
+        lista?.layoutManager = layoutManager
+
+        adaptador = AdaptadorCustom(platillos)
     }
 }
--- app/src/main/java/tech/inversa/ejemplorecyclerview/Platillo.kt
+++ app/src/main/java/tech/inversa/ejemplorecyclerview/Platillo.kt
@@ -1,4 +1,17 @@
 package tech.inversa.ejemplorecyclerview
 
-class Platillo {
+import android.widget.ImageView
+
+class Platillo(nombre: String, precio: Double, rating: Double, foto: Int) {
+    var nombre = ""
+    var precio =  0.0
+    var rating = 0.0
+    var foto = 0
+
+    init {
+        this.nombre = nombre
+        this.precio = precio
+        this.rating = rating
+        this.foto = foto
+    }
 }
